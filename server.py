e981e2edd94203a74523537ad9107901a799553e
BaruYogesh <bhargav.yogesh@gmail.com>
2023-03-10 14:57:03 -0500

    replaced server

--- server.py
+++ server.py
@@ -1,57 +1,43 @@
-from flask import Flask, request
-from flask_socketio import SocketIO, join_room, leave_room, send, emit
-import random
-import db
+from fastapi import FastAPI, WebSocket, Request
+from repos import *
 from models import *
 
-app = Flask(__name__)
-client = db.get_database()
-socketio = SocketIO(app)
+app = FastAPI()
 
-if __name__ == "__main__":
-    socketio.run(app)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-@app.get('/')
-def hello_world():
-    return 'hello world'
+@app.post(
+    "/order", 
+    response_model= Room
+)
+async def order_post(create: RoomCreate) -> Room:
+    return RoomRepo.create(create)
 
-@app.get("/toppings")
-def toppings():
-    pass  # return all toppings
+@app.get(
+    "/order/{order_id}",
+    response_model=Room
+)
+async def order_read(order_id: str) -> Room:
+    return RoomRepo.get(order_id)
 
+@app.patch(
+    "/order/add_member/{order_id}"
+)
+async def order_add_member(order_id: str, update: RoomUpdate):
+    RoomRepo.update(order_id, update)
 
-@app.route("/order", methods=["POST", "PUT", "DELETE"])
-def order():
-    print('order')
-    if request.method == "POST": # organizer creates an order
-        order_codes = client['rooms']
+@app.post(
+    "/user",
+    response_model=User
+)
+async def user_post(create: UserCreate) -> User:
+    return UserRepo.create(create)
 
-        new_code = make_code()
-        while order_codes.find_one({'room_code': new_code}) is not None:
-            new_code = make_code()
-
-        new_room = order_codes.insert_one({
-            'room_code': new_code
-        })
-
-        return {'room_code': new_code}
-
-    elif request.method == "PUT":
-        pass  # user joins a room
-    elif request.method == "DELETE":
-        pass  # organizer closes a room
-
-
-@socketio.on("join")
-def on_join(data):
-    username = data["username"]
-    room = data["room"]
-    join_room(room)
-    send(username + " has entered the room.", to=room)
-
-@socketio.on("leave")
-def on_leave(data):
-    username = data["username"]
-    room = data["room"]
-    leave_room(room)
-    send(username + " has left the room.", to=room)
+@app.get(
+    '/user/{user_id}',
+    response_model=User
+)
+async def user_read(user_id: str) -> User:
+    return UserRepo.get(user_id)
