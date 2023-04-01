bafb1b54681200848379d7e687e9349cc12dc900
BaruYogesh <bhargav.yogesh@gmail.com>
2023-04-01 16:51:52 -0400

    removed pandas import

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 from pymongo.collection import Collection
-from pandas import DataFrame
 
 client = MongoClient("localhost", 27017)
 collection: Collection = client["mypi"]
